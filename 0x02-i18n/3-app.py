7b1631a6af4a3cf8fcc95cd300372ae3dbdf5dcf
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-25 22:23:34 +0000

    Tasks

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Parametrize templates
+"""
+from flask import Flask
+from flask_babel import Babel
+from flask import render_template, request
+
+
+class Config:
+    """
+    Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    _ or gettext function to parametrize your templates
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
+    """
+    The home/index page.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
